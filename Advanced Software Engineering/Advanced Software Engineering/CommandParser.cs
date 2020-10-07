fa70df21196645719f5948e4c36528e131c2cbc6
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-10-07 23:46:50 +0100

    Started working on the CommandParser class

---
+++ Advanced Software Engineering/Advanced Software Engineering/CommandParser.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Software_Engineering
+{
+    class CommandParser
+    {
+
+        Graphics graphics;
+        List<Command> commands = new List<Command>();
+
+
+        void DrawCommands()
+        {
+
+        }
+
+        CommandParser(Graphics graphics)
+        {
+            this.graphics = graphics;
+        }
+
+        CommandParser(Graphics graphics, String rawCommands)
+        {
+            this.graphics = graphics;
+            this.ProcessCommands(rawCommands);
+        }
+
+        void AddCommand(Command command)
+        {
+            commands.Add(command);
+        }
+
+        void ProcessCommands(String rawCommands)
+        {
+            //ignore case
+            rawCommands = rawCommands.ToLower();
+            //isolate commands
+            String[] commands = rawCommands.Split(Environment.NewLine.ToCharArray());
+
+        }
+
+    }
+
+    class Command
+    {
+        
+
+    }
+
+    class Verb
+    {
+
+
+    }
+
+    class Value
+    {
+
+
+    }
+
+}
