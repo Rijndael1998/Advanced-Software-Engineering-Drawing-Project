cc1c44a6b303c91239eb050ef694dbd9e543ec58
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-11-01 19:49:05 +0000

    Documented DetailsWindow

--- Advanced Software Engineering/Advanced Software Engineering/Windows/DetailsWindow.cs
+++ Advanced Software Engineering/Advanced Software Engineering/Windows/DetailsWindow.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace Advanced_Software_Engineering {
+    /// <summary>
+    /// This form is reponsible for showing details. It is essentially a textbox in a form.
+    /// </summary>
     public partial class DetailsWindow : Form {
-        public DetailsWindow() {
+        /// <summary>
+        /// A details window dedicated to showing details.
+        /// </summary>
+        /// <param name="details">The textbox text</param>
+        public DetailsWindow(string details) {
             InitializeComponent();
+            textBox1.Text = details;
         }
 
-        public DetailsWindow(string errorText) {
+        /// <summary>
+        /// A details window that shows details in the textbox and title as the window title.
+        /// </summary>
+        /// <param name="details">the details to be shown in the textbox</param>
+        /// <param name="title">the title of the window</param>
+        public DetailsWindow(string details, string title) {
             InitializeComponent();
-            textBox1.Text = errorText;
-        }
-
-        public DetailsWindow(string errorText, string title) {
-            InitializeComponent();
-            textBox1.Text = errorText;
+            textBox1.Text = details;
             Text = title;
         }
     }
