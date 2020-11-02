f9169e72bd0ed7630334993ff021070055ffa695
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-11-02 19:49:21 +0000

    Documented Dot

--- Advanced Software Engineering/Advanced Software Engineering/Verbs/DrawingVerbs/Dot.cs
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/DrawingVerbs/Dot.cs
@@ -5,16 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Advanced_Software_Engineering.Verbs.DrawingVerbs {
+    /// <summary>
+    /// The Dot Verb class
+    /// </summary>
     public class Dot : Verb {
         Drawer drawer;
+
+        /// <summary>
+        /// Creates the Dot Verb
+        /// </summary>
+        /// <param name="drawer"></param>
         public Dot(Drawer drawer) {
             this.drawer = drawer;
         }
 
+        /// <summary>
+        /// Draws a dot where the pen currently is.
+        /// </summary>
         public void ExecuteVerb() {
             drawer.DrawDot();
         }
 
+        /// <summary>
+        /// Gets the description of the command
+        /// </summary>
+        /// <returns>The description of the command</returns>
         public string GetDescription() {
             return "Places a dot where the pen currently is";
         }
