2c186f1f1dbfee4ae6dd23dd7cd825298f7d993d
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-11-02 19:49:52 +0000

    Documented FillColor

--- Advanced Software Engineering/Advanced Software Engineering/Verbs/DrawingVerbs/FillColor.cs
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/DrawingVerbs/FillColor.cs
@@ -7,20 +7,35 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Advanced_Software_Engineering.Verbs.DrawingVerbs {
+    /// <summary>
+    /// The FillColor Verb class
+    /// </summary>
     public class FillColor : Verb {
 
         Drawer drawer;
         Color color;
 
+        /// <summary>
+        /// Create the FillColor instance
+        /// </summary>
+        /// <param name="drawer">drawer</param>
+        /// <param name="color">The color of the fill</param>
         public FillColor(Drawer drawer, Color color) {
             this.drawer = drawer;
             this.color = color;
         }
 
+        /// <summary>
+        /// Set the fill color
+        /// </summary>
         public void ExecuteVerb() {
             drawer.SetFillColor(color);
         }
 
+        /// <summary>
+        /// Get description of the color that the drawer will use
+        /// </summary>
+        /// <returns>Description of the fill color</returns>
         public string GetDescription() {
             return "Set fill color to " + color.ToString();
         }
