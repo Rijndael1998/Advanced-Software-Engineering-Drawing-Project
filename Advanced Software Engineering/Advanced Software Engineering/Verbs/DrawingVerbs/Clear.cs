e0d1a9a1799922fe1e9febe00db14e7ea5cb0cba
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-11-02 19:49:15 +0000

    Documented Clear

--- Advanced Software Engineering/Advanced Software Engineering/Verbs/DrawingVerbs/Clear.cs
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/DrawingVerbs/Clear.cs
@@ -5,15 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Advanced_Software_Engineering.Verbs.DrawingVerbs {
+    /// <summary>
+    /// The Clear Verb class.
+    /// </summary>
     public class Clear : Verb {
         Drawer drawer;
 
+        /// <summary>
+        /// Create the clear Verb
+        /// </summary>
+        /// <param name="drawer"></param>
         public Clear(Drawer drawer) {
             this.drawer = drawer;
         }
 
+        /// <summary>
+        /// Execute the clear verb. It clears the drawer.
+        /// </summary>
         public void ExecuteVerb() => drawer.ClearCanvas();
 
+        /// <summary>
+        /// Describes the clearing.
+        /// </summary>
+        /// <returns>A description of clearing</returns>
         public string GetDescription() {
             return "Clears the canvas";
         }
