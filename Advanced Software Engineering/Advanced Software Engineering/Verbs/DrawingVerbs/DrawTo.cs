fed5315308606bfe9aa7dd8715d85e5b3a47b1ab
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-11-02 19:49:40 +0000

    Documented DrawTo

--- Advanced Software Engineering/Advanced Software Engineering/Verbs/DrawingVerbs/DrawTo.cs
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/DrawingVerbs/DrawTo.cs
@@ -6,19 +6,36 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Advanced_Software_Engineering.Verbs.DrawingVerbs {
+    /// <summary>
+    /// DrawTo Verb class
+    /// </summary>
     public class DrawTo : Verb {
-        protected Drawer drawer;
-        protected Point moveToPoint;
 
+        Drawer drawer;
+        Point moveToPoint;
+
+        /// <summary>
+        /// Creates the DrawTo intstance
+        /// </summary>
+        /// <param name="drawer">drawer</param>
+        /// <param name="x">X position to move to</param>
+        /// <param name="y">Y position to move to</param>
         public DrawTo(Drawer drawer, int x, int y) {
             this.drawer = drawer;
             moveToPoint = new Point(x, y);
         }
 
+        /// <summary>
+        /// Draws the line
+        /// </summary>
         public void ExecuteVerb() {
             drawer.DrawLine(moveToPoint);
         }
 
+        /// <summary>
+        /// Describes how the line is going to be drawn
+        /// </summary>
+        /// <returns></returns>
         public string GetDescription() {
             return "Draw line " + moveToPoint.X + ", " + moveToPoint.Y;
         }
