243c955b2aa0e8ff51bf72ba43c14cc71e730157
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-11-03 13:02:48 +0000

    Documented ResetPen

--- Advanced Software Engineering/Advanced Software Engineering/Verbs/DrawingVerbs/ResetPen.cs
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/DrawingVerbs/ResetPen.cs
@@ -5,17 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Advanced_Software_Engineering.Verbs.DrawingVerbs {
+    /// <summary>
+    /// Setsets the pen to the initial position.
+    /// </summary>
     public class ResetPen : Verb {
         Drawer drawer;
 
+        /// <summary>
+        /// Creates the ResetPen verb
+        /// </summary>
+        /// <param name="drawer">drawer</param>
         public ResetPen(Drawer drawer) {
             this.drawer = drawer;
         }
 
+        /// <summary>
+        /// Moves the pen to 0, 0
+        /// </summary>
         public void ExecuteVerb() => drawer.MovePen(new System.Drawing.Point(0, 0));
 
+        /// <summary>
+        /// Gets the description of the command.
+        /// </summary>
+        /// <returns>The description of the command</returns>
         public string GetDescription() {
-            return "Moves pen to the start";
+            return "Moves pen to the start (0, 0)";
         }
 
     }
