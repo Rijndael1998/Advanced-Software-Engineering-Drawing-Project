8bccded5973e0113444429f432217a1363cfc04c
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-11-15 05:22:52 +0000

    Removed ValueHelper functions from SettingsAndHelperFunctionsTester

--- Advanced Software Engineering/Advanced Software Engineering Tests/SettingsAndHelperFunctionsTester.cs
+++ Advanced Software Engineering/Advanced Software Engineering Tests/SettingsAndHelperFunctionsTester.cs
@@ -1,5 +1,4 @@
 using Advanced_Software_Engineering;
-using Advanced_Software_Engineering.Verbs;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
@@ -8,7 +7,7 @@ using System.Linq;
 namespace Advanced_Software_Engineering_Tests {
 
     /// <summary>
-    /// This class tests <see cref="SettingsAndHelperFunctions"/> methods and some <see cref="ValueHelper"/> methods. It uses a combination of manual and random tests.
+    /// This class tests <see cref="SettingsAndHelperFunctions"/> methods. It uses a combination of manual and random tests.
     /// </summary>
     [TestClass]
     public class SettingsAndHelperFunctionsTester {
@@ -56,58 +55,6 @@ namespace Advanced_Software_Engineering_Tests {
             string expected = "a    simple    sentence     surrounded    by    spaces";
 
             Assert.AreEqual(expected, SettingsAndHelperFunctions.Strip(input));
-        }
-
-        /// <summary>
-        /// Random <see cref="ValueHelper.ConvertToInt(string)"/> test.
-        /// </summary>
-        [TestMethod]
-        public void ConvertToInt_Random1() {
-            for (int i = 0; i < 100000; i++) {
-                int expected = rand.Next(int.MinValue, int.MaxValue);
-                string input = expected.ToString();
-
-                Assert.AreEqual(expected, ValueHelper.ConvertToInt(input));
-            }
-        }
-
-        /// <summary>
-        /// Random <see cref="ValueHelper.ConvertToDouble(string)"/> test.
-        /// </summary>
-        [TestMethod]
-        public void ConvertToDouble_Random1() {
-            for (int i = 0; i < 100000; i++) {
-                double expected = rand.NextDouble();
-                string input = expected.ToString();
-
-                Assert.AreEqual(expected, ValueHelper.ConvertToDouble(input));
-            }
-        }
-
-        /// <summary>
-        /// Random <see cref="ValueHelper.ConvertToDouble(string)"/> test.
-        /// </summary>
-        [TestMethod]
-        public void ConvertToDouble_Random2() {
-            for (int i = 0; i < 100000; i++) {
-                double expected = rand.Next(100, 100) * rand.NextDouble();
-                string input = expected.ToString();
-
-                Assert.AreEqual(expected, ValueHelper.ConvertToDouble(input));
-            }
-        }
-
-        /// <summary>
-        /// Random <see cref="ValueHelper.ConvertToDouble(string)"/> test.
-        /// </summary>
-        [TestMethod]
-        public void ConvertToDouble_Random3() {
-            for (int i = 0; i < 100000; i++) {
-                int expected = rand.Next(100, 100);
-                string input = ((double)expected).ToString();
-
-                Assert.AreEqual((double)expected, ValueHelper.ConvertToDouble(input));
-            }
         }
 
         /// <summary>
---
+++ Advanced Software Engineering/Advanced Software Engineering Tests/ValueHelperTester.cs
@@ -0,0 +1,66 @@
+﻿using Advanced_Software_Engineering.Verbs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Advanced_Software_Engineering_Tests {
+
+    /// <summary>
+    /// This class tests <see cref="ValueHelper"/> methods. It uses a combination of manual and random tests.
+    /// </summary>
+    [TestClass]
+    internal class ValueHelperTester {
+        private Random rand = new Random();
+
+        /// <summary>
+        /// Random <see cref="ValueHelper.ConvertToInt(string)"/> test.
+        /// </summary>
+        [TestMethod]
+        public void ConvertToInt_Random1() {
+            for (int i = 0; i < 100000; i++) {
+                int expected = rand.Next(int.MinValue, int.MaxValue);
+                string input = expected.ToString();
+
+                Assert.AreEqual(expected, ValueHelper.ConvertToInt(input));
+            }
+        }
+
+        /// <summary>
+        /// Random <see cref="ValueHelper.ConvertToDouble(string)"/> test.
+        /// </summary>
+        [TestMethod]
+        public void ConvertToDouble_Random1() {
+            for (int i = 0; i < 100000; i++) {
+                double expected = rand.NextDouble();
+                string input = expected.ToString();
+
+                Assert.AreEqual(expected, ValueHelper.ConvertToDouble(input));
+            }
+        }
+
+        /// <summary>
+        /// Random <see cref="ValueHelper.ConvertToDouble(string)"/> test.
+        /// </summary>
+        [TestMethod]
+        public void ConvertToDouble_Random2() {
+            for (int i = 0; i < 100000; i++) {
+                double expected = rand.Next(100, 100) * rand.NextDouble();
+                string input = expected.ToString();
+
+                Assert.AreEqual(expected, ValueHelper.ConvertToDouble(input));
+            }
+        }
+
+        /// <summary>
+        /// Random <see cref="ValueHelper.ConvertToDouble(string)"/> test.
+        /// </summary>
+        [TestMethod]
+        public void ConvertToDouble_Random3() {
+            for (int i = 0; i < 100000; i++) {
+                int expected = rand.Next(100, 100);
+                string input = ((double)expected).ToString();
+
+                Assert.AreEqual((double)expected, ValueHelper.ConvertToDouble(input));
+            }
+        }
+    }
+}
