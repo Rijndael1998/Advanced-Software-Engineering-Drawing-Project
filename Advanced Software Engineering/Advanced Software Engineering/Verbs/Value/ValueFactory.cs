91100673c8b5ac833e8f27685091de76110516d8
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-11-15 06:12:53 +0000

    Added basic Value types

--- Advanced Software Engineering/Advanced Software Engineering/Verbs/Value/ValueFactory.cs
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/Value/ValueFactory.cs
@@ -1,4 +1,11 @@
-﻿namespace Advanced_Software_Engineering.Verbs.Value {
+﻿using System;
+
+namespace Advanced_Software_Engineering.Verbs.Value {
     class ValueFactory {
+
+        ValueFactory(string value, Type TypeHint = null) {
+
+        }
+
     }
 }
---
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/Value/ValueTypes/ColorValue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Software_Engineering.Verbs.Value.ValueTypes {
+    class ColorValue : IValue {
+        public string GetDescription() {
+            throw new NotImplementedException();
+        }
+
+        public Type GetOriginalType() {
+            throw new NotImplementedException();
+        }
+
+        public bool ToBool() {
+            throw new NotImplementedException();
+        }
+
+        public Color ToColor() {
+            throw new NotImplementedException();
+        }
+
+        public double ToDouble() {
+            throw new NotImplementedException();
+        }
+
+        public int ToInt() {
+            throw new NotImplementedException();
+        }
+    }
+}
