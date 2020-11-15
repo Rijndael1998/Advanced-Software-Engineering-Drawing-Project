73d6f406396fdf092902fd97ae794d8e6bb6b38d
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-11-15 05:39:03 +0000

    Added IValue interface

---
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/IValue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+
+namespace Advanced_Software_Engineering.Verbs {
+
+    internal interface IValue {
+
+        string GetDescription();
+
+        int ToInt();
+
+        double ToDouble();
+
+        bool ToBool();
+
+        Color ToColor();
+
+        Type GetOriginalType();
+    }
+}
