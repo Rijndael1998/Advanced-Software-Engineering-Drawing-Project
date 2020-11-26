5663aca3eab136a7705ff319a2de441760771b60
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-11-26 12:33:37 +0000

    Better InitialiseVariable is now DeclareVariable

---
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/Value/DeclareVariable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Advanced_Software_Engineering.Verbs.Value {
+
+    public class DeclareVariable : IVerb {
+        private Drawer drawer;
+        private IValue value;
+        private string name;
+
+        public DeclareVariable(Drawer drawer, string type, string assgnment) {
+            this.drawer = drawer;
+
+            value = ValueFactory.CreateValue(type, assgnment);
+        }
+
+        void IVerb.ExecuteVerb() {
+        }
+
+        string IVerb.GetDescription() {
+            throw new NotImplementedException();
+        }
+    }
+}
