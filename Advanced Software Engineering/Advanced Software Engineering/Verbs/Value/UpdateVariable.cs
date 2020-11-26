7a4070220dba3814a2017853a92d0769206c6930
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-11-26 12:34:30 +0000

    Added UpdateVariable class

---
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/Value/UpdateVariable.cs
@@ -0,0 +1,28 @@
+﻿namespace Advanced_Software_Engineering.Verbs.Value {
+
+    public class UpdateVariable : IVerb {
+        protected Drawer drawer;
+        protected string name;
+        protected string value;
+
+        public UpdateVariable(Drawer drawer, string name, string value) {
+            this.name = name;
+            this.value = value;
+            this.drawer = drawer;
+        }
+
+        public UpdateVariable(Drawer drawer, string name, IValue value) {
+
+        }
+
+        public void ExecuteVerb() {
+            IValue variable = drawer.GetVariable(name);
+            string type = variable.GetOriginalType();
+            drawer.SetVariable(name, ValueFactory.CreateValue(value, type));
+        }
+
+        public string GetDescription() {
+            return "Updates " + name + " to " + value;
+        }
+    }
+}
