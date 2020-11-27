50bb6811cd445dc058741e2118cad5a682c3105d
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-11-27 14:20:21 +0000

    Added VariableValue

--- Advanced Software Engineering/Advanced Software Engineering/Verbs/Value/ValueTypes/VariableValue.cs
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/Value/ValueTypes/VariableValue.cs
@@ -7,14 +7,15 @@ using System.Threading.Tasks;
 
 namespace Advanced_Software_Engineering.Verbs.Value.ValueTypes {
     class VariableValue : IValue {
-
-        static Dictionary<string, IValue> variables = new Dictionary<string, IValue>();
-
         public string GetDescription() {
             throw new NotImplementedException();
         }
 
-        public Type GetOriginalType() {
+        public string GetOriginalType() {
+            throw new NotImplementedException();
+        }
+
+        public bool isInitialised() {
             throw new NotImplementedException();
         }
 
