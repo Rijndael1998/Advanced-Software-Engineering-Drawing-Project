46804ab01dfb4c8ca9abc39bc2f3cca5c673506e
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-11-27 19:47:02 +0000

    Added PointValue objects for IValue

---
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/Value/ValueObjects/PointValue.cs
@@ -0,0 +1,28 @@
+﻿using Advanced_Software_Engineering.Verbs.Value.ValueTypes;
+using System.Drawing;
+
+namespace Advanced_Software_Engineering.Verbs.Value.Value_Objects {
+
+    public class PointValue {
+        private IValue x;
+        private IValue y;
+
+        public PointValue(int x, int y) {
+            this.x = new IntValue(x);
+            this.y = new IntValue(y);
+        }
+
+        public PointValue(IValue x, IValue y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int GetX() {
+            return x.ToInt();
+        }
+
+        public int GetY() {
+            return y.ToInt();
+        }
+    }
+}
