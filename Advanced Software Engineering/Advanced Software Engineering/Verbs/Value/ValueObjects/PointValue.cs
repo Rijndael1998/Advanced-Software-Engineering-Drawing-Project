e49732b1c530184b421a83e8f8e26541f2bc92dd
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-11-27 21:01:21 +0000

    Added better constructor

--- Advanced Software Engineering/Advanced Software Engineering/Verbs/Value/ValueObjects/PointValue.cs
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/Value/ValueObjects/PointValue.cs
@@ -1,7 +1,7 @@
 ﻿using Advanced_Software_Engineering.Verbs.Value.ValueTypes;
 using System.Drawing;
 
-namespace Advanced_Software_Engineering.Verbs.Value.Value_Objects {
+namespace Advanced_Software_Engineering.Verbs.Value.ValueObjects {
 
     public class PointValue {
         private IValue x;
@@ -17,12 +17,21 @@ namespace Advanced_Software_Engineering.Verbs.Value.Value_Objects {
             this.y = y;
         }
 
+        public PointValue(string parameter1, string parameter2) {
+            x = ValueFactory.CreateValue(parameter1, "int");
+            y = ValueFactory.CreateValue(parameter2, "int");
+        }
+
         public int GetX() {
             return x.ToInt();
         }
 
         public int GetY() {
             return y.ToInt();
+        } 
+
+        public Point GetPoint() {
+            return new Point(GetX(), GetY());
         }
     }
 }
