5844e489dae93d1fc15d2f3acdc1afb045919f75
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-11-29 10:05:56 +0000

    Created comparison value

---
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/Value/ValueTypes/ComparisonValue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+
+namespace Advanced_Software_Engineering.Verbs.Value.ValueTypes {
+
+    internal class ComparisonValue : IValue {
+        private string op;
+        private IValue value1;
+        private IValue value2;
+
+        public ComparisonValue(IValue value1, IValue value2, string op) {
+            this.value1 = value1;
+            this.value2 = value2;
+
+            switch (op) {
+                case "=":
+                case "!":
+                case ">":
+                case "<":
+                    break;
+
+                default:
+                    throw new Exception("Invalid operation");
+            }
+
+            this.op = op;
+        }
+
+        public string GetDescription() {
+            return "Compare two values";
+        }
+
+        public string GetOriginalType() {
+            return "comparison";
+        }
+
+        public bool isInitialised() {
+            return true;
+        }
+
+        public bool ToBool() {
+            switch (op) {
+                case "=":
+                    return value1.ToBool() == value2.ToBool() && value1.ToDouble() == value2.ToDouble() && value1.ToInt() == value2.ToInt();
+
+                case "!":
+                    return value1.ToBool() != value2.ToBool() && value1.ToDouble() != value2.ToDouble() && value1.ToInt() != value2.ToInt();
+
+                case ">":
+                    return value1.ToDouble() > value2.ToDouble() && value1.ToInt() > value2.ToInt();
+
+                case "<":
+                    return value1.ToDouble() < value2.ToDouble() && value1.ToInt() < value2.ToInt();
+
+                default:
+                    throw new Exception("Bad operator");
+            }
+        }
+
+        public Color ToColor() {
+            throw new Exception("Cannot convert comparison to color");
+        }
+
+        public double ToDouble() {
+            throw new Exception("Cannot convert comparison to double");
+        }
+
+        public int ToInt() {
+            throw new Exception("Cannot convert comparison to int");
+        }
+    }
+}
