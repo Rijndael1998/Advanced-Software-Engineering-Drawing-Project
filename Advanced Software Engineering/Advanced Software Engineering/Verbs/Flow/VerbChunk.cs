07e4c04ae9167e98192ec9cee7938f800cd56312
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-11-29 14:41:03 +0000

    Created VerbChunks

---
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/Flow/VerbChunk.cs
@@ -0,0 +1,20 @@
+﻿namespace Advanced_Software_Engineering.Verbs.Flow {
+
+    public class VerbChunk : IVerb {
+        private readonly IVerb[] verbs;
+
+        public VerbChunk(IVerb[] verbs) {
+            this.verbs = verbs;
+        }
+
+        public void ExecuteVerb() {
+            foreach (IVerb verb in verbs) verb.ExecuteVerb();
+        }
+
+        public string GetDescription() {
+            string s = "VerbChunk Start";
+            foreach (IVerb verb in verbs) s += "\n" + verb.GetDescription();
+            return s + "\n" + "VerbChunk End";
+        }
+    }
+}
