1d9fed93a1515cc6b8983d0f64076c25b1453709
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-11-30 09:29:23 +0000

    Created IfChunk

--- Advanced Software Engineering/Advanced Software Engineering/Verbs/Flow/IfChunk.cs
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/Flow/IfChunk.cs
@@ -1,7 +1,17 @@
-﻿namespace Advanced_Software_Engineering.Verbs.Flow {
-    class IfChunk : VerbChunk, IVerb {
+﻿using Advanced_Software_Engineering.Verbs.Value;
 
+namespace Advanced_Software_Engineering.Verbs.Flow {
+    public class IfChunk : VerbChunk, IVerb {
 
+        IValue conditional;
+
+        public IfChunk(IValue conditional) : base() {
+            this.conditional = conditional;
+        }
+
+        public void ExecuteVerb() {
+            if (conditional.ToBool()) base.ExecuteVerb();
+        }
 
     }
 }
