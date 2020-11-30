ffc58914695925b725d11c225af2faa9ea9f79b3
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-11-30 12:12:36 +0000

    Added a NoOp action

---
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/DrawingVerbs/Actions/NoOp.cs
@@ -0,0 +1,11 @@
+﻿namespace Advanced_Software_Engineering.Verbs.DrawingVerbs.Actions {
+    class NoOp : IVerb {
+        public void ExecuteVerb() {
+
+        }
+
+        public string GetDescription() {
+            return "NoOp";
+        }
+    }
+}
