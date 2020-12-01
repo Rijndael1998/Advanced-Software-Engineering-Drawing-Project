92566266eecb249471f71c4a8a0835bb6df46759
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-12-01 21:49:41 +0000

    Added MethodChunk

---
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/Flow/MethodChunk.cs
@@ -0,0 +1,50 @@
+﻿using Advanced_Software_Engineering.Verbs.Value;
+using Advanced_Software_Engineering.Verbs.Value.ValueTypes;
+using System.Drawing;
+
+namespace Advanced_Software_Engineering.Verbs.Flow {
+    public class MethodChunk : VerbChunk, IVerb, IValue {
+
+        ValueStorage valueStorage;
+        VariableValue[] variableValues;
+
+        public MethodChunk(VariableValue[] variableValues, ValueStorage valueStorage) : base() {
+            this.valueStorage = valueStorage;
+            this.variableValues = variableValues;
+        }
+
+        public new void ExecuteVerb() {
+            valueStorage.IncreaseStack();
+            base.ExecuteVerb();
+            valueStorage.DecreaseStack();
+        }
+
+        public IValue Clone() {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetOriginalType() {
+            throw new System.NotImplementedException();
+        }
+
+        public bool isInitialised() {
+            throw new System.NotImplementedException();
+        }
+
+        public bool ToBool() {
+            throw new System.NotImplementedException();
+        }
+
+        public Color ToColor() {
+            throw new System.NotImplementedException();
+        }
+
+        public double ToDouble() {
+            throw new System.NotImplementedException();
+        }
+
+        public int ToInt() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
