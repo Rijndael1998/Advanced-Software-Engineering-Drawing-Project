ab37eaf3d08775e8a1a520d40459c82c062d1b2b
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-12-04 17:10:34 +0000

    Documented IVerbChunk

--- Advanced Software Engineering/Advanced Software Engineering/Verbs/Flow/IVerbChunk.cs
+++ Advanced Software Engineering/Advanced Software Engineering/Verbs/Flow/IVerbChunk.cs
@@ -1,11 +1,23 @@
 ﻿namespace Advanced_Software_Engineering.Verbs.Flow {
-
+    /// <summary>
+    /// A Collection of chunks to be executed. They extend IVerb because they should be interchangable. 
+    /// </summary>
     public interface IVerbChunk : IVerb {
-
+        /// <summary>
+        /// Adds a verb to the verb list
+        /// </summary>
+        /// <param name="verb">The verb to add</param>
         void AddVerb(IVerb verb);
 
-        void ExecuteVerb();
+        /// <summary>
+        /// Execute the whole chunk
+        /// </summary>
+        new void ExecuteVerb();
 
-        string GetDescription();
+        /// <summary>
+        /// Gets the description of what the chunk will do
+        /// </summary>
+        /// <returns>A description of the chunk's actions</returns>
+        new string GetDescription();
     }
 }
